c32cc70f17c2b6c5a057543110c4395412b1f459
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2024-01-08 08:11:36 -0800

    Fix checker errors for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,12 +5,3 @@ def print_matrix_integer(matrix=[]):
         for col in row:
             print("{:d}".format(col), end=" ")
         print()
-
-# Example usage:
-my_matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(my_matrix)
-
